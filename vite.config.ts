cf2f3c7380f0eb14a7da7d463d562258f273e308
Ragib Ahsan <ragib.ahsan@gmail.com>
2026-04-10 20:56:47 -0700

    add support for https cert from custom local pat

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import mkcert from 'vite-plugin-mkcert'
 import { resolve } from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss(), mkcert()],
-  server: {
-    host: true,
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      https: env.LOCAL_CERT ? {
+        cert: env.LOCAL_CERT,
+        key: env.LOCAL_KEY,
+      } : undefined,
+      host: true,
     },
-  },
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'),
+      },
+    },
+  }
 })
